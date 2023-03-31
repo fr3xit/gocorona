7497510153d242330f9ca3fbf5f4916d02881fa6
fr3xit <fr3xit@gmail.com>
2023-03-31 13:09:07 +0300

    31-03-2023:13:09

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const header = document.querySelector('#header');
+const scrollable = document.documentElement;
+
+const toggleMenu = () => {
+	header.classList.toggle('header_active');
+};
+
+const colorizeMenu = () => {
+	console.log('test');
+};
+
+document.querySelector('#burger').addEventListener('click', toggleMenu);
+
+window.addEventListener('scroll', () => {
+	if (scrollable.scrollTop > 0) {
+		header.classList.add('header_colorize');
+	} else {
+		header.classList.remove('header_colorize');
+	}
+});
